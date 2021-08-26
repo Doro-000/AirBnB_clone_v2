ae52ee3b826295265ac237f76aacfb705e8be73e
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-08-27 01:09:10 +0300

    advanced task

---
+++ 100-clean_web_static.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""deletes out-of-date archives"""
+from fabric.api import *
+
+env.hosts = ['34.138.61.3', '34.75.118.132']
+env.user = "ubuntu"
+
+def do_clean(number=0):
+    """deletes out-of-date archives"""
+    with lcd("./versions"):
+        unparsed_result = local("ls -t")
+    if (unparsed_result):
+        result = unparsed_result.split()
+        if (len(result) > 1):
+            if (number == 0 or number == 1):
+                num = 1
+            try:
+                for tar in result[num:]:
+                    local("rm ./versions/{}".format(tar))
+                    name = tar.split(".")[0]
+                    run("rm -rf /data/web_static/releases/{}".format(name))
+            except:
+                pass                
+    else:
+        pass
