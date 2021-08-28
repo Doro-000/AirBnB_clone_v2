626da1c1d665384f0ad528c5a3b9e5e8c8ad833b
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-28 15:48:50 +0300

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,19 +9,31 @@ env.user = "ubuntu"
 def do_clean(number=0):
     """deletes out-of-date archives"""
     number = int(number)
+    if (number == 0 or number == 1):
+        num = 1
+    else:
+        num = number
+
     with lcd("./versions"):
         unparsed_result = local("ls -t .")
+    with cd("/data/web_static/releases"):
+        unparsed_result_2 = run("ls -t .")
+
     if (unparsed_result):
         result = unparsed_result.split()
         if (len(result) > 1):
-            if (number == 0 or number == 1):
-                num = 1
             try:
                 for tar in result[num:]:
-                    local("rm ./versions/{}".format(tar))
-                    name = tar.split(".")[0]
-                    run("rm -rf /data/web_static/releases/{}".format(name))
+                    local("rm -f ./versions/{}".format(tar))
             except:
                 pass
-    else:
-        pass
+
+    if (unparsed_result_2):
+        result_2 = unparsed_result_2.split()
+        if (len(result_2) > 1):
+            try:
+                for file in result_2[num:]:
+                    run("rm -rf /data/web_static/releases/{}".format(file))
+            except:
+                pass
+
