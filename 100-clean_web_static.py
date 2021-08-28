265378eabff58a43e052f3851c558a7922460b1c
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-08-28 16:43:13 +0300

    Update 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """deletes out-of-date archives"""
 from fabric.api import *
+from os import walk
 
 env.hosts = ['34.138.61.3', '34.75.118.132']
 env.user = "ubuntu"
@@ -9,31 +10,28 @@ env.user = "ubuntu"
 def do_clean(number=0):
     """deletes out-of-date archives"""
     number = int(number)
-    if (number == 0 or number == 1):
-        num = 1
-    else:
-        num = number
-
-    with lcd("./versions"):
-        unparsed_result = local("ls -t .")
+    [*unparsed_result] = walk("./versions")
     with cd("/data/web_static/releases"):
         unparsed_result_2 = run("ls -t .")
-
     if (unparsed_result):
-        result = unparsed_result.split()
+        result = unparsed_result[0][2][::-1]
         if (len(result) > 1):
+            if (number == 0 or number == 1):
+                num = 1
+            else:
+                num = number
             try:
                 for tar in result[num:]:
-                    local("rm -f ./versions/{}".format(tar))
+                    with lcd("./versions"):
+                        local("rm -f {}".format(tar))
             except:
                 pass
-
     if (unparsed_result_2):
         result_2 = unparsed_result_2.split()
         if (len(result_2) > 1):
-            try:
-                for file in result_2[num:]:
-                    run("rm -rf /data/web_static/releases/{}".format(file))
-            except:
-                pass
-
+            if (number == 0 or number == 1):
+                num = 1
+            else:
+                num = number
+            for file in result_2[num:]:
+                run("rm -rf /data/web_static/releases/{}".format(file))
