2528588d7288524366fff274f75a6f40e892126f
Hizkyas Tekle <63017001+Doro-000@users.noreply.github.com>
2021-09-13 00:37:45 +0300

    Update 5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
+"""Flask framework
+"""
+from flask import Flask, url_for, render_template
 
-from flask import Flask, render_template
-app = Flask("__name__")
-
-
-@app.route('/', strict_slashes=False)
-def hello():
-        """hello world func"""
-        return "Hello HBNB!"
+app = Flask(__name__)
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-        """hbnb func"""
-        return "HBNB"
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """return hello hbhb
+    """
+    return "Hello HBNB!"
 
 
-@app.route('/c/<string>', strict_slashes=False)
-def c_route(string):
-        """Handles variables"""
-        return "C {}".format(string.replace("_", " "))
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    """return HBNB"""
+    return "HBNB"
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<string>', strict_slashes=False)
-def python_route(string="is cool"):
-        """Handles optional variables"""
-        return "Python {}".format(string.replace("_", " "))
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    """return text given"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python/<text>', strict_slashes=False)
+def display(text):
+    """display “Python ”, followed by the value of the text"""
+    return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def num_route(n):
-        """Handles converted variables"""
-        return "{} is a number".format(n)
+def num_display(n):
+    """display “n is a number” only"""
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def render_num_route(n):
-        """serves static files with variables"""
-        return render_template("5-number.html", name=n)
+def num_html(n):
+    """display HTML is "n" is a number only"""
+    return render_template('5-number.html', name=n)
 
 
 if __name__ == "__main__":
-        app.run()
+    app.run()
