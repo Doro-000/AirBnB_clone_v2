cee2097dcca395c62026589be59cbb3d02ff2005
Hizkyas Tekle <hizkyastekle@gmail.com>
2021-09-17 20:30:36 +0300

    fix question 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,11 +9,13 @@ app = Flask(__name__)
 
 @app.teardown_appcontext
 def refresh(exception):
+        """reload storage when refreshed"""
         models.storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def route_states():
+        """route for states list"""
         pep_fix = models.dummy_classes["State"]
         data = models.storage.all(cls=pep_fix)
         states = data.values()
@@ -22,10 +24,11 @@ def route_states():
 
 @app.route("/cities_by_states", strict_slashes=False)
 def route_city():
+        """route for cities and states"""
         pep_fix = models.dummy_classes["State"]
         states = models.storage.all(cls=pep_fix).values()
         return render_template('8-cities_by_states.html', states_list=states)
 
 
 if __name__ == "__main__":
-        app.run()
+        app.run(host="0.0.0.0", port="5000")
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,28 +4,30 @@
 from flask import Flask, render_template
 import models
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def refresh(exception):
-        models.storage.close()
+    """reload storage when refreshed"""
+    models.storage.close()
 
 
 @app.route("/states", strict_slashes=False)
-def route_states():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('7-states_list.html', states_list=states)
-
-
 @app.route("/states/<id>", strict_slashes=False)
-def route_city():
-        pep_fix = models.dummy_classes["State"]
-        data = models.storage.all(cls=pep_fix)
-        states = data.values()
-        return render_template('8-cities_by_states.html', states_list=states)
+def state_route(id=None):
+    """route for states"""
+    pep_fix = models.dummy_classes["State"]
+    data = models.storage.all(cls=pep_fix)
+    states = None
+    if (id is not None):
+        for key in data:
+            k = key.split(".")[1]
+            if (k == id):
+                states = data["State.{}".format(k)]
+    else:
+        states = list(data.values())
+    return render_template('9-states.html', states_list=states)
 
 
 if __name__ == "__main__":
